71a9e44c01235ccbc3b9f36447c85d6f85e1a6a1
lekrieg <danielhenriquesilva2010@hotmail.com>
2024-05-01 19:08:10 -0300

    basic project structure

---
+++ MinimalAPISolution/src/MinimalAPI/Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Builder;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
